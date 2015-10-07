ce93fe3ffe3e6e0739d5e46587982b8c798dfe45
dimdym <dimdym@gmail.com>
2015-10-07 16:10:16 -0500

    added selection of controller medication compliance metric MRNs

--- Quality/patient-selector.py
+++ Quality/patient-selector.py
@@ -4,7 +4,7 @@ import csv, datetime
 
 CSVFILE = '/Users/dima/Boston/Quality/Asthma/data.csv'
 
-def find_severity_score_patients():
+def severity_score_patients():
   """Patients for severy score compliance metric"""
 
   # 13 months expressed in days
@@ -18,9 +18,25 @@ def find_severity_score_patients():
       start_date = date_of_service - delta
       print line['MRN'], start_date.strftime('%m/%d/%y'), line['DATE_OF_SERVICE']
 
+def controller_medication_compliance():
+  """Patients for controler medication compliance metric"""
+
+  # 12 months expressed in days
+  delta = datetime.timedelta(days = 365)
+  dict_reader = csv.DictReader(open(CSVFILE, "rU"))
+  print 'MRN', 'start_date', 'end_date'
+
+  for line in dict_reader:
+    if line['DATE_OF_LAST_AAP'] == '' and line['SEVERITY_PPM'] == 'Persistent':
+      date_of_service = datetime.datetime.strptime(line['DATE_OF_SERVICE'], '%m/%d/%y')
+      start_date = date_of_service - delta
+      print line['MRN'], start_date.strftime('%m/%d/%y'), line['DATE_OF_SERVICE']
+
 if __name__ == "__main__":
   
-  find_severity_score_patients()
+  controller_medication_compliance()
+
+
 
 
     
