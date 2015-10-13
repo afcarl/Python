8587dfb2e8d8e157cb0f9db2856a9d5509f35670
dimdym <dimdym@gmail.com>
2015-10-13 16:10:16 -0500

    now generating document vectors

--- Quality/vectorize.py
+++ Quality/vectorize.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -B 
 import collections, os
 
-PATH = '/Users/Dima/Desktop/Text/'
+PATH = '/Users/Dima/Boston/Data/Temp/Asthma/SampleText/'
 
 def read_file(file):
   """Return a file as a list of words"""      
@@ -15,21 +15,36 @@ def read_file(file):
   return words
 
 def make_alphabet():
-  """Do a pass over corpus and generate unique words"""
+  """Do a pass over corpus and map all unique words to dimensions"""
   
   word_counts = collections.Counter()
   for file in os.listdir(PATH):
     words = read_file(PATH + file)
     word_counts.update(words)
 
-  alphabet = []
+  # libsvm indexes start from 1
+  index = 1
+  # key: word, value: index
+  alphabet = collections.OrderedDict() 
   for word, count in word_counts.items():
-    if count <= 10:
-      alphabet.append(word)
+    if count > 10:
+      alphabet[word] = index
+      index = index + 1
+  
+  return alphabet
 
-  print len(alphabet), "\n"
-  print alphabet
+def make_vectors(alphabet):
+  """Convert documents to vectors"""
+
+  for file in os.listdir(PATH):
+    vector = []
+    document_unique_words = set(read_file(PATH + file))
+    for word, index in alphabet.items():
+      if word in document_unique_words:
+        vector.append("%s:%s" % (index, 1))
+    print ' '.join(vector)
 
 if __name__ == "__main__":
 
-  make_alphabet()
+  alphabet = make_alphabet()
+  make_vectors(alphabet)
