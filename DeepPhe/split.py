eb1077815f7271f9d6101c68387ea368fcb526bb
dimdym <dimdym@gmail.com>
2015-10-23 10:57:50 -0400

    modified to extract training files instead of dev files

--- DeepPhe/split.py
+++ DeepPhe/split.py
@@ -4,6 +4,7 @@ import os, shutil
 
 ALL = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/All/'
 DEV = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/Dev/'
+TRAIN = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/Train/'
 
 TRAINPATIENTS = set(['03', '11', '92', '93'])
 DEVPATIENTS = set(['02', '21'])
@@ -12,7 +13,7 @@ TESTPATIENTS = set(['01', '16'])
 if __name__ == "__main__":
 
   for dir in os.listdir(ALL):
-    for patient_num in DEVPATIENTS:
+    for patient_num in TRAINPATIENTS:
       prefix = 'patient' + patient_num
       if dir.startswith(prefix):
-        shutil.copytree(ALL + dir, DEV + dir)
+        shutil.copytree(ALL + dir, TRAIN + dir)
