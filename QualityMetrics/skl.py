b0cdc8f2b58451088562363845000a06e7925c6b
dimdym <dimdym@gmail.com>
2015-11-23 12:40:39 -0600

    first draft of a scikit-learn based classifier

---
+++ QualityMetrics/skl.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python -B
+
+import sklearn
+import sklearn.datasets
+import sklearn.feature_extraction.text
+import sklearn.cross_validation
+import sklearn.naive_bayes
+
+NOTES = '/Users/dima/Boston/Data/QualityMetrics/Text'
+
+def main():
+  """ """      
+
+  bunch = sklearn.datasets.load_files(NOTES)
+  
+  # raw occurences
+  vectorizer = sklearn.feature_extraction.text.CountVectorizer()
+  counts = vectorizer.fit_transform(bunch.data)
+  
+  # tf-idf 
+  tf = sklearn.feature_extraction.text.TfidfTransformer()
+  tfidf = tf.fit_transform(counts)
+
+  folds = sklearn.cross_validation.KFold(len(bunch.data), n_folds=5)
+  for train_indices, test_indices in folds:
+    train_x = tfidf[train_indices]
+    train_y = bunch.target[train_indices]
+    test_x = tfidf[test_indices]
+    test_y = bunch.target[test_indices]
+    classifier = sklearn.naive_bayes.MultinomialNB().fit(train_x, train_y)
+    acc = classifier.score(test_x, test_y)
+    print acc
+
+if __name__ == "__main__":
+
+  main()
