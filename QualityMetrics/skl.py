692ff29d9d3ba38f157efb46628f7c5eb5524789
dimdym <dimdym@gmail.com>
2015-11-24 12:10:19 -0600

    switched SVC to LinearSVC (SVC was using a non-linear kernel); added init params for count vectorizer

--- QualityMetrics/skl.py
+++ QualityMetrics/skl.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python -B
 
-import sklearn
+import sklearn as sk
+import numpy as np
 import sklearn.datasets
 import sklearn.feature_extraction.text
 import sklearn.cross_validation
 import sklearn.naive_bayes
+import sklearn.svm
 
-NOTES = '/Users/dima/Boston/Data/QualityMetrics/Text'
+NOTES = '/Users/dima/Boston/Data/QualityMetrics/Balanced/'
 
 def main():
   """ """      
 
-  # bunch = sklearn.datasets.load_files(NOTES)
-  categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-  bunch = sklearn.datasets.fetch_20newsgroups(subset='train', categories=categories)
+  bunch = sk.datasets.load_files(NOTES)
 
   # raw occurences
-  vectorizer = sklearn.feature_extraction.text.CountVectorizer()
+  vectorizer = sk.feature_extraction.text.CountVectorizer(
+    ngram_range=(2, 2), 
+    stop_words='english',
+    min_df=3)  
   counts = vectorizer.fit_transform(bunch.data)
   
   # tf-idf 
-  tf = sklearn.feature_extraction.text.TfidfTransformer()
+  tf = sk.feature_extraction.text.TfidfTransformer()
   tfidf = tf.fit_transform(counts)
-
-  folds = sklearn.cross_validation.KFold(len(bunch.data), n_folds=5)
+  
+  scores = []
+  folds = sk.cross_validation.KFold(len(bunch.data), n_folds=5)
   for train_indices, test_indices in folds:
     train_x = tfidf[train_indices]
     train_y = bunch.target[train_indices]
     test_x = tfidf[test_indices]
     test_y = bunch.target[test_indices]
-    classifier = sklearn.naive_bayes.MultinomialNB().fit(train_x, train_y)
-    acc = classifier.score(test_x, test_y)
-    print acc
+    # classifier = sk.naive_bayes.MultinomialNB().fit(train_x, train_y)
+    classifier = sk.svm.LinearSVC().fit(train_x, train_y)
+    scores.append(classifier.score(test_x, test_y))
+  
+  print np.mean(scores)
 
 if __name__ == "__main__":
 
