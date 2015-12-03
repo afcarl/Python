ace685a40b37510aeca77ad84b2aeba2fc413790
dimdym <dimdym@gmail.com>
2015-12-03 10:48:03 -0600

    added path to test dir

--- DeepPhe/split.py
+++ DeepPhe/split.py
@@ -5,6 +5,7 @@ import os, shutil
 ALL = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/All/'
 DEV = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/Dev/'
 TRAIN = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/Train/'
+TEST = '/Users/dima/Boston/Data/DeepPhe/Metastasis/Anafora/Test/'
 
 TRAINPATIENTS = set(['03', '11', '92', '93'])
 DEVPATIENTS = set(['02', '21'])
@@ -13,7 +14,7 @@ TESTPATIENTS = set(['01', '16'])
 if __name__ == "__main__":
 
   for dir in os.listdir(ALL):
-    for patient_num in TRAINPATIENTS:
+    for patient_num in TESTPATIENTS:
       prefix = 'patient' + patient_num
       if dir.startswith(prefix):
-        shutil.copytree(ALL + dir, TRAIN + dir)
+        shutil.copytree(ALL + dir, TEST + dir)
