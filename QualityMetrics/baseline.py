d368a43da23fde6ef0ef0e3863782d369a8a0383
dimdym <dimdym@gmail.com>
2015-12-08 11:05:55 -0600

    previous version was wrong

--- QualityMetrics/baseline.py
+++ QualityMetrics/baseline.py
@@ -10,9 +10,8 @@ VOCABULARY = ['severe asthma', 'persistent asthma', 'mild asthma',
 def counting():
   """Count the number of times each vocabulary item is seen"""
 
-  counts = {} # key: label, total count of vocabulary items
-  counts[CLASSDIRS[0]] = 0
-  counts[CLASSDIRS[1]] = 0
+  correct = 0
+  incorrect = 0
 
   for label in CLASSDIRS:
     for file in os.listdir(locations.BALANCED + label):
@@ -20,7 +19,13 @@ def counting():
       text = open(path).read()
       for bigram in VOCABULARY:
         if bigram in text:
-          counts[label] = counts[label] + 1
+          # baselined labeled this document as a 'yes'
+          if label == 'Yes':
+            correct = correct + 1
+          else:
+            incorrect = incorrect + 1
+
+  print correct, incorrect, float(correct) / (correct + incorrect)
 
 if __name__ == "__main__":
 
