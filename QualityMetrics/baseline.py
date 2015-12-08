00ec4f57642b1316962115c8086cfe47dadddcc7
dimdym <dimdym@gmail.com>
2015-12-08 11:29:26 -0600

    previous version wasn't right either; this one is!

--- QualityMetrics/baseline.py
+++ QualityMetrics/baseline.py
@@ -4,7 +4,7 @@ import locations
 import os
 
 CLASSDIRS = ['Yes', 'No']
-VOCABULARY = ['severe asthma', 'persistent asthma', 'mild asthma', 
+KEYPHRASES = ['severe asthma', 'persistent asthma', 'mild asthma', 
               'intermittent asthma', 'moderate asthma']
 
 def counting():
@@ -15,15 +15,26 @@ def counting():
 
   for label in CLASSDIRS:
     for file in os.listdir(locations.BALANCED + label):
+
       path = os.path.join(locations.BALANCED, label, file)
-      text = open(path).read()
-      for bigram in VOCABULARY:
-        if bigram in text:
-          # baselined labeled this document as a 'yes'
-          if label == 'Yes':
-            correct = correct + 1
-          else:
-            incorrect = incorrect + 1
+      document_text = open(path).read()
+      key_phrase_seen = False
+      for key_phrase in KEYPHRASES:
+        if key_phrase in document_text:
+          key_phrase_seen = True
+
+      if key_phrase_seen:
+        # baselined labeled this document as a 'yes'
+        if label == 'Yes':
+          correct = correct + 1
+        else:
+          incorrect = incorrect + 1
+      else:
+        # baseline labeled this document as a 'no'
+        if label == 'Yes':
+          incorrect = incorrect + 1
+        else:
+          correct = correct + 1
 
   print correct, incorrect, float(correct) / (correct + incorrect)
 
