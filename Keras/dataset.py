d4bf1c519ea230b1421eca2cdcd8d5147d9ae673
dimdym <dimdym@gmail.com>
2016-04-02 12:18:32 -0500

    added method to convert sentences to lists of indices

--- Keras/dataset.py
+++ Keras/dataset.py
@@ -6,30 +6,44 @@ PATH = '/Users/Dima/Loyola/Data/RtPolarity/rt-polarity.*'
 
 class Dataset:
   """Interface to RT sentiment data"""
-
+  
   def __init__(self):
     """ """
 
+    self.alphabet = {} # key: unigram, value: index
+
   def make_alphabet(self):
-    """ """
+    """Words indexed by overall frequency in the dataset"""
 
     unigrams = []
     for file_name in glob.glob(PATH):
-      print 'reading file:', file_name
       for line in open(file_name):
         printable = ''.join(c for c in line if c in string.printable)
         unigrams.extend(printable.split())
 
-    alphabet = {} # key: unigram, value: index
-    index = 1     # zero used to encode unknown words
+    index = 1 # zero used to encode unknown words
     unigram_counts = collections.Counter(unigrams)
     for unigram, count in unigram_counts.most_common():
-      alphabet[unigram] = index
+      self.alphabet[unigram] = index
       index = index + 1
 
-    return alphabet
+  def get_examples(self):
+    """Indices instead of words"""
+
+    all_examples = []
+    for file_name in glob.glob(PATH):
+      for line in open(file_name):
+        one_example = []
+        printable = ''.join(c for c in line if c in string.printable)
+        for unigram in printable.split():
+          one_example.append(self.alphabet[unigram])
+        all_examples.append(one_example)
+
+    return all_examples
 
 if __name__ == "__main__":
 
   dataset = Dataset()
   dataset.make_alphabet()
+  examples = dataset.get_examples()
+  print examples[6]
