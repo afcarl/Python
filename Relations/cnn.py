76a1ceb1276ea772a798b57536606466b8b512e0
dimdym <dimdym@gmail.com>
2016-05-24 16:24:55 -0500

    moved settings to properties file

--- Relations/cnn.py
+++ Relations/cnn.py
@@ -12,28 +12,20 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution1D, MaxPooling1D
 from keras.layers.embeddings import Embedding
-
 import sys
 sys.path.append('../Lib/')
 import word2vec_model
+import properties
   
-batch = 25
-epochs = 5
-embdims = 300
-filters = 200
-filtlen = 2
-
-emb_path = '/Users/Dima/Loyola/Data/Word2Vec/Models/mimic.txt'
-data_path = '/Users/Dima/Loyola/Data/Thyme/Deep/Rel/both.txt'
 train_size = 78452 # first 78452 indices
 
 if __name__ == "__main__":
 
-  dataset = dataset.DatasetProvider(data_path)
+  dataset = dataset.DatasetProvider(properties.data_path)
   x, y = dataset.load_data()
 
   # what happens with OOV words / index 0?
-  word2vec = word2vec_model.Model(emb_path)
+  word2vec = word2vec_model.Model(properties.emb_path)
   init_vectors = word2vec.select_vectors(dataset.alphabet)
 
   # turn x and y into numpy array among other things
@@ -58,12 +50,12 @@ if __name__ == "__main__":
   model = k.models.Sequential()
     
   model.add(Embedding(len(dataset.alphabet),
-                      embdims,
+                      properties.embdims,
                       input_length=maxlen,
                       weights=[init_vectors]))
 
-  model.add(Convolution1D(nb_filter=filters,
-                          filter_length=filtlen,
+  model.add(Convolution1D(nb_filter=properties.filters,
+                          filter_length=properties.filtlen,
                           border_mode='valid',
                           activation='relu',
                           subsample_length=1))
@@ -78,8 +70,8 @@ if __name__ == "__main__":
                 metrics=['accuracy'])
   model.fit(train_x,
             train_y,
-            nb_epoch=epochs,
-            batch_size=batch,
+            nb_epoch=properties.epochs,
+            batch_size=properties.batch,
             verbose=1,
             validation_split=0.1)
 
---
+++ Relations/properties.py
@@ -0,0 +1,8 @@
+emb_path = '/Users/Dima/Loyola/Data/Word2Vec/Models/mimic.txt'
+data_path = '/Users/Dima/Loyola/Data/Thyme/Deep/Rel/both.txt'
+
+batch = 25
+epochs = 5
+embdims = 300
+filters = 200
+filtlen = 2
