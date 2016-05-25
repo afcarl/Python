dc4dfe9b9b889b9bb1c338b5e724e2f5b800011e
dimdym <dimdym@gmail.com>
2016-05-25 08:41:33 -0500

    removed properties file from version control

--- Relations/properties.py
+++
@@ -1,8 +0,0 @@
-emb_path = '/Users/Dima/Loyola/Data/Word2Vec/Models/mimic.txt'
-data_path = '/Users/Dima/Loyola/Data/Thyme/Deep/Rel/both.txt'
-
-batch = 25
-epochs = 5
-embdims = 300
-filters = 200
-filtlen = 2
