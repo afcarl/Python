13e6c3778cca697ca862150e59f84293abc8d844
dimdym <dimdym@gmail.com>
2016-07-11 11:58:10 -0500

    now labels read from corpus rather than hardcoded

--- Lib/dataset.py
+++ Lib/dataset.py
@@ -9,46 +9,33 @@ import ConfigParser
 
 import glob, string, collections, operator
 
-label2int = {
-  'none':0,
-  'contains':1,
-  'contains-1':2
-  }
-
-# will have to do this eventually
-# label2int = {
-#   'none': 0,
-#   'contains': 1,
-#   'contains-1': 2,
-#   'before': 3,
-#   'before-1': 4,
-#   'begins-on': 5,
-#   'begins-on-1': 6,
-#   'ends-on': 7,
-#   'ends-on-1': 8,
-#   'overlap': 9,
-#   'overlap-1': 10,
-# }
-
 class DatasetProvider:
   """THYME relation data"""
   
   def __init__(self, file_names):
     """Index words by frequency in a list of files"""
 
-    self.alphabet = {} # words indexed by frequency
-
-    unigrams = [] # read entire corpus into a list
+    self.word2int = {} # words indexed by frequency
+    self.label2int = {}   # class to int mapping
+    
+    unigrams = [] # corpus as list
+    labels = []   # classes as list
     for file_name in file_names:
       for line in open(file_name):
         label, text = line.strip().split('|')
         unigrams.extend(text.split())
-
+        labels.append(label)
+        
     index = 1 # zero used to encode unknown words
+    self.word2int['oov_word'] = 0
     unigram_counts = collections.Counter(unigrams)
-    self.alphabet['oov_word'] = 0
     for unigram, count in unigram_counts.most_common():
-      self.alphabet[unigram] = index
+      self.word2int[unigram] = index
+      index = index + 1
+
+    index = 0 # index classes
+    for label in set(labels):
+      self.label2int[label] = index
       index = index + 1
 
   def load(self, path):
@@ -60,9 +47,9 @@ class DatasetProvider:
       label, text = line.strip().split('|')
       example = []
       for unigram in text.split():
-        example.append(self.alphabet[unigram])
+        example.append(self.word2int[unigram])
       examples.append(example)
-      labels.append(label2int[label])
+      labels.append(self.label2int[label])
 
     return examples, labels
 
@@ -73,13 +60,13 @@ if __name__ == "__main__":
 
   dataset = DatasetProvider([cfg.get('data', 'train'),
                              cfg.get('data', 'test')])
-  print 'alphabet size:', len(dataset.alphabet)
+  print 'alphabet size:', len(dataset.word2int)
 
   x,y = dataset.load(cfg.get('data', 'test'))
 
   print 'max seq len:', max([len(s) for s in x])
   print 'number of examples:', len(x)
-  print 'number of labels:', len(set(y))
+  print 'labels:', dataset.label2int
   print 'label counts:', collections.Counter(y)
   print 'first 10 examples:', x[:10]
   print 'class proportions:'
