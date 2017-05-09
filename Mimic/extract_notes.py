7574dbbd2797a99f951e123dd96c3ec8656f482c
dimdym <dimdym@gmail.com>
2017-05-09 10:40:11 -0500

    added a function to write patient files

--- Mimic/extract_notes.py
+++ Mimic/extract_notes.py
@@ -1,12 +1,22 @@
-#! /Library/Frameworks/Python.framework/Versions/2.7/bin/python -B
-
+#! /usr/bin/env python
 import pandas, string, os
 
-NOTES_CSV = '/Users/Dima/Loyola/Data/MimicIII/NOTEEVENTS.csv'
-OUT_DIR = 'Text/'
+NOTES_CSV = '/Users/Dima/Loyola/Data/MimicIII/Source/NOTEEVENTS.csv'
+OUT_DIR = '/Users/Dima/Loyola/Data/MimicIII/PatientVec/Patients/'
 BATCH = 500000
 
-if __name__ == "__main__":
+def write_to_single_dir():
+  """Write files to one directory. Group by patient."""
+
+  frame = pandas.read_csv(NOTES_CSV)
+
+  for row_id, subj_id, text in zip(frame.ROW_ID, frame.SUBJECT_ID, frame.TEXT):
+    outfile = open('%s%s.txt' % (OUT_DIR, subj_id), 'a')
+    # outfile.write('* * * * * ROW_ID: %s * * * * * \n\n' % row_id)
+    outfile.write(text)
+
+def write_to_multiple_dirs():
+  """Split input into files grouped in several directories"""
 
   data_frame = pandas.read_csv(NOTES_CSV)
 
@@ -15,3 +25,7 @@ if __name__ == "__main__":
     outfile = open('%s/%s.txt' % (out_dir, id), 'w')
     printable = ''.join(c for c in text if c in string.printable)
     outfile.write(printable)
+
+if __name__ == "__main__":
+
+  write_to_single_dir()
