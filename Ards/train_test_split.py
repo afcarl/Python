e053b113d5fe0abb9f6ea1acb81a4dcdb108fcf6
dimdym <dimdym@gmail.com>
2017-06-27 12:03:49 -0500

    re-org

--- Ards/train_test_split.py
+++ Ards/train_test_split.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python                                                                     
+#!/usr/bin/env python
 import numpy as np
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
@@ -9,14 +9,14 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
-notes_root = '/Users/Dima/Loyola/Mount/ards/text'
+notes_root = '/Users/Dima/Loyola/Data/Ards/Cuis/'
 feature_list = './features.txt'
-num_folds = 5
-ngram_range = (1, 2)
+# ngram_range = (1, 2)
+ngram_range = (1, 1)
 min_df = 50
 
-def run_cross_validation():
-  """Run n-fold CV and return average accuracy"""      
+def run_experiment():
+  """Split into train and test and run an experiment"""
 
   bunch = load_files(notes_root)
   print 'positive class:', bunch.target_names[1]
@@ -24,38 +24,38 @@ def run_cross_validation():
 
   # raw occurences
   vectorizer = CountVectorizer(
-    ngram_range=ngram_range, 
+    ngram_range=ngram_range,
     stop_words='english',
     min_df=min_df ,
     vocabulary=None,
     binary=False)
   count_matrix = vectorizer.fit_transform(bunch.data)
-  
+
   # print features to file for debugging
   feature_file = open(feature_list, 'w')
   for feature in vectorizer.get_feature_names():
     feature_file.write(feature + '\n')
-  
-  # tf-idf 
+
+  # tf-idf
   tf = TfidfTransformer()
   tfidf_matrix = tf.fit_transform(count_matrix)
 
-  x_train, x_test, y_train, y_test = train_test_split(
-    tfidf_matrix, bunch.target, test_size = 0.1, random_state=0)
+  x_train, x_test, y_train, y_test = train_test_split(tfidf_matrix,
+                                                      bunch.target,
+                                                      test_size = 0.2,
+                                                      random_state=1)
 
   classifier = LinearSVC(class_weight='balanced')
   model = classifier.fit(x_train, y_train)
   predicted = classifier.predict(x_test)
-  print 'predictions:', predicted
-  print
 
   precision = precision_score(y_test, predicted, pos_label=1)
   recall = recall_score(y_test, predicted, pos_label=1)
   f1 = f1_score(y_test, predicted, pos_label=1)
   print 'p =', precision
   print 'r =', recall
-  print 'f1 =', f1  
+  print 'f1 =', f1
 
 if __name__ == "__main__":
 
-  run_cross_validation()
+  run_experiment()
